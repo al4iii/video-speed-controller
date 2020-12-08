1ff9a35d7e85b58e40460301b2c2cfc8ddd875a4
Aliaksandr Danilin <al4icrux@gmail.com>
2020-12-08 11:01:05 +0300

    fix: after checking by a mentor;

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const speed = document.querySelector('.speed');
 const bar = speed.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
 
-function handleMove(e) {
-    const y = e.pageY - this.offsetTop;
-    const percent = y / this.offsetHeight;
-    const min = 0.4;
-    const max = 4;
-    const height = Math.round(percent * 100) + '%';
-    const playbackRate = percent * (max - min) + min;
-    bar.style.height = height;
-    bar.textContent = playbackRate.toFixed(2) + '×';
-    video.playbackRate = playbackRate;
-  }
+let handleMove = (event) => {
+  const y = event.pageY - speed.offsetTop;    
+  const percent = y / speed.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + `%`;
+  const playbackRate = percent * (max - min) + min;
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + ` ×`;
+  video.playbackRate = playbackRate;
+}
 
 speed.addEventListener('mousemove', handleMove);
